918590dbb4afc68fb6ece1333e65b5470d15c085
Tamas Cservenak <tamas@cservenak.net>
2025-01-27 21:46:14 +0100

    More test

--- node/file/src/test/java/eu/maveniverse/maven/mimir/node/file/FileNodeTest.java
+++ node/file/src/test/java/eu/maveniverse/maven/mimir/node/file/FileNodeTest.java
@@ -1,5 +1,6 @@
 package eu.maveniverse.maven.mimir.node.file;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -33,7 +34,7 @@ public class FileNodeTest {
             new SimpleKeyMapperFactory().createKeyMapper(Config.defaults().build());
 
     @Test
-    void smoke(@TempDir Path basedir) throws Exception {
+    void smoke(@TempDir Path basedir, @TempDir Path workdir) throws Exception {
         Config config = Config.defaults().basedir(basedir).build();
         try (FileNode fileNode = new FileNodeFactory(
                         Map.of(SimpleKeyResolverFactory.NAME, new SimpleKeyResolverFactory()),
@@ -64,6 +65,49 @@ public class FileNodeTest {
                     "861844d6704e8573fec34d967e20bcfef3d424cf48be04e6dc08f2bd58c729743371015ead891cc3cf1c9d34b49264b510751b1ff9e537937bc46b5d6ff4ecc8",
                     systemEntry.checksums().get(Sha512ChecksumAlgorithmFactory.NAME));
             System.out.println(systemEntry.metadata());
+
+            Path target = workdir.resolve("target.file");
+            systemEntry.transferTo(target);
+            assertArrayEquals(Files.readAllBytes(target), data);
+        }
+    }
+
+    @Test
+    void smokeNoLink(@TempDir Path basedir, @TempDir Path workdir) throws Exception {
+        Config config = Config.defaults().basedir(basedir).setUserProperty("mimir.file.mayLink", "false").build();
+        try (FileNode fileNode = new FileNodeFactory(
+                Map.of(SimpleKeyResolverFactory.NAME, new SimpleKeyResolverFactory()),
+                Map.of(
+                        Sha1ChecksumAlgorithmFactory.NAME,
+                        new Sha1ChecksumAlgorithmFactory(),
+                        Sha512ChecksumAlgorithmFactory.NAME,
+                        new Sha512ChecksumAlgorithmFactory()))
+                .createNode(config)) {
+            Optional<FileEntry> entry = fileNode.locate(keyMapper.apply(central, junit));
+            assertFalse(entry.isPresent());
+
+            byte[] data = "Hello World!".getBytes(StandardCharsets.UTF_8);
+            Path temp = Files.createTempFile("mimir", "tmp");
+            Files.write(temp, data, StandardOpenOption.TRUNCATE_EXISTING);
+            Map<String, String> checksums = ChecksumAlgorithmHelper.calculate(
+                    data, Arrays.asList(new Sha1ChecksumAlgorithmFactory(), new Sha512ChecksumAlgorithmFactory()));
+            fileNode.store(keyMapper.apply(central, junit), temp, checksums);
+
+            entry = fileNode.locate(keyMapper.apply(central, junit));
+            assertTrue(entry.isPresent());
+            SystemEntry systemEntry = entry.orElseThrow();
+            assertEquals("12", systemEntry.metadata().get(SystemEntry.CONTENT_LENGTH));
+            assertEquals(
+                    "2ef7bde608ce5404e97d5f042f95f89f1c232871",
+                    systemEntry.checksums().get(Sha1ChecksumAlgorithmFactory.NAME));
+            assertEquals(
+                    "861844d6704e8573fec34d967e20bcfef3d424cf48be04e6dc08f2bd58c729743371015ead891cc3cf1c9d34b49264b510751b1ff9e537937bc46b5d6ff4ecc8",
+                    systemEntry.checksums().get(Sha512ChecksumAlgorithmFactory.NAME));
+            System.out.println(systemEntry.metadata());
+
+            Path target = workdir.resolve("target.file");
+            systemEntry.transferTo(target);
+            assertArrayEquals(Files.readAllBytes(target), data);
         }
     }
 }
