9016153615df3f835b8b89d6d5d85cd37013193d
Tamas Cservenak <tamas@cservenak.net>
2025-03-05 14:07:39 +0100

    Make it work

---
+++ it/extension-its/src/it/no-http-traffic/verify.groovy
@@ -0,0 +1,6 @@
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
+
+String log = buildLog.text
+assert !log.contains ('RETRIEVED=0') // cache was primed, it must use it
+assert log.contains('CACHED=0/0') // cache was primed, nothing should be stored
