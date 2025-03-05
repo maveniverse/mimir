cc3261157e1162e7ffe294905d38be1c3abebc1a
Tamas Cservenak <tamas@cservenak.net>
2025-03-05 14:30:40 +0100

    Add mvn4 IT

--- it/extension-its/src/it/no-http-traffic/verify.groovy
+++ it/extension-its/src/it/no-http-traffic/verify.groovy
@@ -2,5 +2,13 @@ File buildLog = new File( basedir, 'second.log' )
 assert buildLog.exists()
 
 String log = buildLog.text
-assert !log.contains ('RETRIEVED=0') // cache was primed, it must use it
-assert log.contains('CACHED=0/0') // cache was primed, nothing should be stored
+
+// Lets make strict assertion
+// Explanation: cache was primed, so aside of "tail repo" everything else was located and retrieved, and
+// nothing should be cached as cache was primed.
+// Also Maven 3 vs 4 diff: they resolve differently
+if (log.contains('Apache Maven 3.9.9')) {
+    assert log.contains('[INFO] Mimir session closed (LOCATED=203/203 RETRIEVED=203 CACHED=0/0)')
+} else if (log.contains('Apache Maven 4.0.0')) {
+    assert log.contains('[INFO] Mimir session closed (LOCATED=195/195 RETRIEVED=195 CACHED=0/0)')
+}
