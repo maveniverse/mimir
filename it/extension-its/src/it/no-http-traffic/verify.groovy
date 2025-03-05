c57bbe47fb2d0074c61c34f75e3260acb270ca5d
Tamas Cservenak <tamas@cservenak.net>
2025-03-05 14:43:24 +0100

    Do not play with logs, something is fishy

--- it/extension-its/src/it/no-http-traffic/verify.groovy
+++ it/extension-its/src/it/no-http-traffic/verify.groovy
@@ -1,10 +1,7 @@
-File firstLog = new File( basedir, 'first.log' )
-assert firstLog.exists()
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
 
-File secondLog = new File( basedir, 'second.log' )
-assert secondLog.exists()
-
-String log = secondLog.text
+String log = buildLog.text
 
 // Lets make strict assertion
 // Explanation: cache was primed, so aside of "tail repo" everything else was located and retrieved, and
@@ -14,4 +11,6 @@ if (log.contains('Apache Maven 3.9.9')) {
     assert log.contains('[INFO] Mimir session closed (LOCATED=203/203 RETRIEVED=203 CACHED=0/0)')
 } else if (log.contains('Apache Maven 4.0.0')) {
     assert log.contains('[INFO] Mimir session closed (LOCATED=195/195 RETRIEVED=195 CACHED=0/0)')
+} else {
+    throw new IllegalStateException("What Maven version is this?")
 }
