14d22b7f35c5b4efab8fd31affbbadc1d4ddf96f
Tamas Cservenak <tamas@cservenak.net>
2025-04-01 22:43:01 +0200

    Drop unused

--- node/daemon/src/test/java/eu/maveniverse/maven/mimir/node/daemon/protocol/ProtocolTest.java
+++ node/daemon/src/test/java/eu/maveniverse/maven/mimir/node/daemon/protocol/ProtocolTest.java
@@ -1,16 +1,20 @@
 package eu.maveniverse.maven.mimir.node.daemon.protocol;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.junit.jupiter.api.Test;
 
 public class ProtocolTest {
     @Test
     void smoke() throws Exception {
+        CopyOnWriteArrayList<Message> messages = new CopyOnWriteArrayList<>();
         try (ServerSocket serverSocket = new ServerSocket(0);
                 Socket clientSck = new Socket("localhost", serverSocket.getLocalPort());
                 Socket serverSck = serverSocket.accept()) {
@@ -19,8 +23,7 @@ public class ProtocolTest {
                         try {
                             Handle handle = new Handle(clientSck.getOutputStream(), clientSck.getInputStream());
                             handle.writeRequest(Request.hello(Map.of("hello", "world")));
-                            Response response = handle.readResponse();
-                            System.out.println(response);
+                            messages.add(handle.readResponse());
                         } catch (IOException e) {
                             fail(e);
                         }
@@ -31,7 +34,7 @@ public class ProtocolTest {
                         try {
                             Handle handle = new Handle(serverSck.getOutputStream(), serverSck.getInputStream());
                             Request request = handle.readRequest();
-                            System.out.println(request);
+                            messages.add(request);
                             handle.writeResponse(Response.okMessage(request, "hi!"));
                         } catch (IOException e) {
                             fail(e);
@@ -45,5 +48,17 @@ public class ProtocolTest {
             server.join();
             client.join();
         }
+
+        assertEquals(2, messages.size());
+        Message first = messages.get(0);
+        Message second = messages.get(1);
+
+        assertInstanceOf(Request.class, first, first.toString());
+        assertInstanceOf(Response.class, second, second.toString());
+
+        assertEquals(Request.CMD_HELLO, ((Request) first).cmd());
+        assertEquals("world", first.requireData("hello"));
+        assertEquals(Response.STATUS_OK, ((Response) second).status());
+        assertEquals("hi!", second.requireData(Response.DATA_MESSAGE));
     }
 }
