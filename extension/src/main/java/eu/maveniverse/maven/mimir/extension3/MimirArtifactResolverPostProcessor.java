7e391ec2ca1fc3a414a41405d838231639a73f2e
Tamas Cservenak <tamas@cservenak.net>
2025-04-02 20:59:37 +0200

    Mimir resolver postprocessor
    WIP

---
+++ extension/src/main/java/eu/maveniverse/maven/mimir/extension3/MimirArtifactResolverPostProcessor.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mimir.extension3;
+
+import java.util.List;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.spi.resolution.ArtifactResolverPostProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Named
+public class MimirArtifactResolverPostProcessor implements ArtifactResolverPostProcessor {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void postProcess(RepositorySystemSession session, List<ArtifactResult> artifactResults) {
+        for (ArtifactResult artifactResult : artifactResults) {
+            logger.info(
+                    "{} @ {}",
+                    artifactResult.getArtifact(),
+                    artifactResult.getRequest().getRequestContext());
+        }
+    }
+}
