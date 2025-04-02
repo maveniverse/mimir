9ccbadbba8b8b2505eb1a1538dcd7eeefd218fb4
Tamas Cservenak <tamas@cservenak.net>
2025-04-02 16:37:11 +0200

    Move "simple" things to their place (#56)
    Right now iface offered as default methods the simple methods
    and simple class just inherited them. This was misleading.
    
    Move simple code to simple classes, and make them public static
    so possible to extend if needed.

--- core/src/main/java/eu/maveniverse/maven/mimir/shared/naming/KeyMapper.java
+++ core/src/main/java/eu/maveniverse/maven/mimir/shared/naming/KeyMapper.java
@@ -11,28 +11,5 @@ import java.net.URI;
 import java.util.function.BiFunction;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.repository.RemoteRepository;
-import org.eclipse.aether.util.artifact.ArtifactIdUtils;
 
-public interface KeyMapper extends BiFunction<RemoteRepository, Artifact, URI> {
-    /**
-     * Provides default and simplistic "container" implementation.
-     */
-    default String container(RemoteRepository repository) {
-        return repository.getId();
-    }
-
-    /**
-     * Provides default and simplistic "name" implementation.
-     */
-    default String name(Artifact artifact) {
-        return ArtifactIdUtils.toId(artifact);
-    }
-
-    /**
-     * Creates a cache key according to naming strategy.
-     */
-    @Override
-    default URI apply(RemoteRepository remoteRepository, Artifact artifact) {
-        return URI.create("mimir:artifact:" + container(remoteRepository) + ":" + name(artifact));
-    }
-}
+public interface KeyMapper extends BiFunction<RemoteRepository, Artifact, URI> {}
