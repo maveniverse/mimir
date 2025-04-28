2abafa90a25c6585998391b1ee0b3967ffb7ef02
Tamas Cservenak <tamas@cservenak.net>
2025-04-28 22:24:42 +0200

    Fix dumps

--- it/extension-its/src/it/no-http-traffic/verify.groovy
+++ it/extension-its/src/it/no-http-traffic/verify.groovy
@@ -2,33 +2,32 @@ File buildLog = new File( basedir, 'build.log' )
 assert buildLog.exists()
 String build = buildLog.text
 
-
 File firstLog = new File( basedir, 'first.log' )
-assert firstLog.exists()
+assert firstLog.exists() : build
 String first = firstLog.text
 
 File secondLog = new File( basedir, 'second.log' )
-assert secondLog.exists()
+assert secondLog.exists() : first
 String second = secondLog.text
 
 File thirdLog = new File( basedir, 'third.log' )
-assert thirdLog.exists()
+assert thirdLog.exists() : second
 String third = thirdLog.text
 
 // Lets make strict assertion
 // Also, consider Maven 3 vs 4 diff: they resolve differently; do not assert counts
 
 // first run: both were empty: retrieved==0 cached!=0
-assert first.contains('[INFO] Mimir session closed') : build
-assert first.contains('RETRIEVED=0') : build
-assert first.contains('CACHED=') && !first.contains('CACHED=0') : build
+assert first.contains('[INFO] Mimir session closed')
+assert first.contains('RETRIEVED=0')
+assert first.contains('CACHED=') && !first.contains('CACHED=0')
 
 // second run: mimir is primed, local repo is empty: retrieved!=0 cached==0
-assert second.contains('[INFO] Mimir session closed') : build
-assert second.contains('RETRIEVED=') && !second.contains('RETRIEVED=0') : build
-assert second.contains('CACHED=0') : build
+assert second.contains('[INFO] Mimir session closed')
+assert second.contains('RETRIEVED=') && !second.contains('RETRIEVED=0')
+assert second.contains('CACHED=0')
 
 // third run: mimir is empty, local repo is primed: retrieved==0 cached!=0
-assert third.contains('[INFO] Mimir session closed') : build
-assert third.contains('RETRIEVED=0') : build
-assert third.contains('CACHED=') && !third.contains('CACHED=0') : build
+assert third.contains('[INFO] Mimir session closed')
+assert third.contains('RETRIEVED=0')
+assert third.contains('CACHED=') && !third.contains('CACHED=0')
