7131e034a47c801882c3228a36f15bc8dc806257
Tamas Cservenak <tamas@cservenak.net>
2025-05-31 20:05:56 +0200

    Log a bit more

--- core/src/main/java/eu/maveniverse/maven/mimir/shared/impl/SessionFactoryImpl.java
+++ core/src/main/java/eu/maveniverse/maven/mimir/shared/impl/SessionFactoryImpl.java
@@ -75,6 +75,7 @@ public final class SessionFactoryImpl extends ComponentSupport implements Sessio
         if (logger.isDebugEnabled()) {
             logger.debug("Mimir {} session created", config.mimirVersion());
             logger.debug("  Enabled: {}", config.enabled());
+            logger.debug("  Basedir: {}", config.basedir());
             logger.debug("  Properties: {}", config.basedir().resolve(config.propertiesPath()));
             logger.debug("  Key mapper: {}", keyMapper.getClass().getSimpleName());
             logger.debug("  Local Node: {}", localNode);
