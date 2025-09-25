73fdd2a1967fc5610d763abb2849a17dd03de910
Tamas Cservenak <tamas@cservenak.net>
2025-09-25 18:35:01 +0200

    Infuser
    A simple help for testing w/ Mimir.

---
+++ testing/src/main/java/eu/maveniverse/maven/mimir/testing/MimirInfuser.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mimir.testing;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Class that sets up Mimir for embedded and chroot-ed maven executing tests; IF outer build uses Mimir as well
+ * (like CI setup).
+ * <p>
+ * Using this class with transparently use outer build used Mimir daemon, contributing whatever it needs to cache, and
+ * enjoying caching benefits.
+ * <p>
+ * Note: this class expects that current property {@code user.home} be the "real" user home (so invocation must happen
+ * outside), and that the passed in path to be the chroot-ed user home.
+ */
+public final class MimirInfuser {
+    public static void infuse(Path chrootUserHome) throws IOException {
+        requireNonNull(chrootUserHome);
+        Path realUserWideExtensions =
+                Path.of(System.getProperty("user.home")).resolve(".m2").resolve("extensions.xml");
+        if (Files.isRegularFile(realUserWideExtensions)) {
+            String realUserWideExtensionsString = Files.readString(realUserWideExtensions);
+            if (realUserWideExtensionsString.contains("<groupId>eu.maveniverse.maven.mimir</groupId>")
+                    && realUserWideExtensionsString.contains("<artifactId>extension3</artifactId>")) {
+                Path chrootUserWideExtensions = chrootUserHome.resolve(".m2").resolve("extensions.xml");
+                // some tests do prepare project and user wide extensions; skip those for now
+                if (!Files.isRegularFile(chrootUserWideExtensions)) {
+                    Files.createDirectories(chrootUserWideExtensions.getParent());
+                    Files.copy(realUserWideExtensions, chrootUserWideExtensions, StandardCopyOption.REPLACE_EXISTING);
+
+                    Path chrootMimirProperties =
+                            chrootUserHome.resolve(".mimir").resolve("session.properties");
+                    Files.createDirectories(chrootMimirProperties.getParent());
+                    String sessionProperties = "# Written by MimirInfuser\n"
+                            + String.format("mimir.daemon.basedir=%s/.mimir\n", System.getProperty("user.home"))
+                            + "mimir.daemon.autoupdate=false\n"
+                            + "mimir.daemon.autostart=false";
+                    Files.writeString(chrootMimirProperties, sessionProperties);
+                }
+            }
+        }
+    }
+}
---
+++ testing/src/test/java/eu/maveniverse/maven/mimir/testing/MimirInfuserTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mimir.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class MimirInfuserTest {
+    @Test
+    void smoke(@TempDir Path tmpDir) throws IOException {
+        Path oldRoot = tmpDir.resolve("oldRoot");
+        Path mimirBasedir = oldRoot.resolve(".mimir");
+        Path extensionsXml = oldRoot.resolve(".m2").resolve("extensions.xml");
+        Files.createDirectories(extensionsXml.getParent());
+        Files.writeString(
+                oldRoot.resolve(".m2").resolve("extensions.xml"),
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<extensions>\n"
+                        + "    <extension>\n"
+                        + "        <groupId>eu.maveniverse.maven.mimir</groupId>\n"
+                        + "        <artifactId>extension3</artifactId>\n"
+                        + "        <version>0.8.0</version>\n"
+                        + "    </extension>\n"
+                        + "</extensions>\n");
+
+        Path newRoot = tmpDir.resolve("newRoot");
+        System.setProperty("user.home", oldRoot.toString());
+        MimirInfuser.infuse(newRoot);
+        Properties props = new Properties();
+        try (InputStream is = Files.newInputStream(newRoot.resolve(".mimir").resolve("session.properties"))) {
+            props.load(is);
+        }
+        assertEquals(3, props.size());
+        assertEquals(mimirBasedir.toString(), props.getProperty("mimir.daemon.basedir"));
+        assertEquals("false", props.getProperty("mimir.daemon.autostart"));
+        assertEquals("false", props.getProperty("mimir.daemon.autoupdate"));
+    }
+}
