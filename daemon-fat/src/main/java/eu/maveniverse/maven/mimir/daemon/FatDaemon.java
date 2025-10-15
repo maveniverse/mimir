e0140db44ff5e2432520c3e4ce8040ada751361e
Tamas Cservenak <tamas@cservenak.net>
2025-10-15 12:47:15 +0200

    Add placeholder

---
+++ daemon-fat/src/main/java/eu/maveniverse/maven/mimir/daemon/FatDaemon.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mimir.daemon;
+
+/**
+ * This is just a placeholder class, as this module has no sources (is merged from daemon-slim and extra nodes).
+ * But, to have sources and javadocs, this placeholder class is here.
+ */
+public class FatDaemon {}
